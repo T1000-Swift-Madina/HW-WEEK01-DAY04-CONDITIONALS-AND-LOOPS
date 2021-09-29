3771c24d1788ae69f3a6d88b5626c4c362044a64
none <Osama.Fallatah.med.s@tuwaiq.edu.sa>
2021-09-29 23:50:40 +0300

    done

---
+++ OsamaHW1D4.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+// int
+
+
+var tstname = "tm"
+var Cities = ["Abha","Badr","Dammam","Jeddah","Jubil","Macca","Madina","Najran","Riyadh"]
+Cities.append("Taif")
+print(Cities)
+var nmpr = [1.09,0.06,1.25,4.69,0.68,2.04,1.48,0.50,7.67,0.69]
+for _ in Cities{
+    switch tstname{
+    case Cities[0]:
+        print(nmpr[0],"m")
+    case Cities[1]:
+        print(nmpr[1],"m")
+    case Cities[2]:
+        print(nmpr[2],"m")
+    case Cities[3]:
+        print(nmpr[3],"m")
+    case Cities[4]:
+        print(nmpr[4],"m")
+    case Cities[5]:
+        print(nmpr[5],"m")
+    case Cities[6]:
+        print(nmpr[6],"m")
+    case Cities[7]:
+        print(nmpr[7],"m")
+    case Cities[8]:
+        print(nmpr[8],"m")
+    case Cities[9]:
+        print(nmpr[9],"m")
+
+    default:
+      print("Chooes city name")   }
+}
+
+    
+    
+
+
+
+// if  tst == ciys[0]  {
+        
+      //  print(nmpr[0])
+
