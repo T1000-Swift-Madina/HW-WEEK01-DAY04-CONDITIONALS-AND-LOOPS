a213b5784b6bd72a7414f642e981f8e83fc45333
yazzan-96 <yazanaraddadi@gmail.com>
2021-09-29 18:08:39 +0300

    new file

---
+++ homework.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+var selctedCity = "taif"
+var cities = [ "medina","jeddah"," makkah","riyadh","dammam","yanbu","tabuk","jazan","hail","taif"]
+var population = [5000,2000,1000,7777,2033,3232,3131,434343,21212,31313,]
+
+if selctedCity == cities [0] {
+   print("the population of \(cities[0]) \(population[0])")
+} else if selctedCity == cities [1] {
+    
+    print("the population of \(cities[1]) \(population[1])")}
+else if  selctedCity == cities [2] {
+        print("the population of \(cities[2]) \(population[2])")}
+else if  selctedCity == cities [3] {
+        print("the population of \(cities[3]) \(population[3])")}
+    
+
+else if selctedCity == cities [4] {
+    print("the population of \(cities[4]) \(population[4])")}
+  else if selctedCity == cities [4] {
+        print("the population of \(cities[5]) \(population[5])")}
+   else if selctedCity == cities [5] {
+            print("the population of \(cities[6]) \(population[6])")}
+  else if selctedCity == cities [6] {
+                print("the population of \(cities[7]) \(population[7])")}
+                else if selctedCity == cities [8] {
+                    print("the population of \(cities[8]) \(population[8])")}
+                    else if selctedCity == cities [9] {
+                        print("the population of \(cities[9]) \(population[9])")}
+                        else  {
+                            print("the population of \(cities[10]) \(population[10])")}
+          
+        
+        
+    
+
+
+
+
+
