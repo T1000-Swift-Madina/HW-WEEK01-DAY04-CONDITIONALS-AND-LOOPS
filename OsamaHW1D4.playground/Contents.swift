02adb354362540cbfd74cb1925deabc86ccf80ab
Osa3a-Fallatah <91524745+Osa3a-Fallatah@users.noreply.github.com>
2021-09-30 11:07:03 +0300

    fixed some error

--- OsamaHW1D4.playground/Contents.swift
+++ OsamaHW1D4.playground/Contents.swift
@@ -1,5 +1,5 @@
 import UIKit
-// int
+
 
 
 var tstname = "tm"
@@ -7,39 +7,34 @@ var Cities = ["Abha","Badr","Dammam","Jeddah","Jubil","Macca","Madina","Najran",
 Cities.append("Taif")
 print(Cities)
 var nmpr = [1.09,0.06,1.25,4.69,0.68,2.04,1.48,0.50,7.67,0.69]
-for _ in Cities{
-    switch tstname{
+
+switch tstname{
+    
     case Cities[0]:
-        print(nmpr[0],"m")
+        print(Cities[0],nmpr[0],"m")
     case Cities[1]:
-        print(nmpr[1],"m")
+        print(Cities[1],nmpr[1],"m")
     case Cities[2]:
-        print(nmpr[2],"m")
+        print(Cities[2],nmpr[2],"m")
     case Cities[3]:
-        print(nmpr[3],"m")
-    case Cities[4]:
-        print(nmpr[4],"m")
+        print(Cities[3],nmpr[3],"m")
+    case Cities[4]
+        :print(Cities[4],nmpr[4],"m")
     case Cities[5]:
-        print(nmpr[5],"m")
+        print(Cities[5],nmpr[5],"m")
     case Cities[6]:
-        print(nmpr[6],"m")
+        print(Cities[6],nmpr[6],"m")
     case Cities[7]:
-        print(nmpr[7],"m")
+        print(Cities[7],nmpr[7],"m")
     case Cities[8]:
-        print(nmpr[8],"m")
+        print(Cities[8],nmpr[8],"m")
     case Cities[9]:
-        print(nmpr[9],"m")
+        print(Cities[9],nmpr[9],"m")
+
 
     default:
       print("Chooes city name")   }
 }
 
     
-    
-
-
-
-// if  tst == ciys[0]  {
-        
-      //  print(nmpr[0])
-
+   
