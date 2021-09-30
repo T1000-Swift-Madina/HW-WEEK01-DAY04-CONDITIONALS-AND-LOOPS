646c150da8e60538dc2dd8fd773614af3e6fc715
AfafSaleh <Afaf.Saleh.med.s@tuwaiq.edu.sa>
2021-09-30 03:23:23 +0300

    HOMEWORK4 ; Done

---
+++ Homework4/HomeWork4:1.playground/Contents.swift
@@ -0,0 +1,69 @@
+import UIKit
+
+var Cities = "Mahkh"
+var citiy = [String]()
+
+citiy.append(" Jeedh ")
+citiy.append(" Mahkh ")
+citiy.append(" Tabuk ")
+citiy.append(" Madinah")
+citiy.append(" Riadh ")
+citiy.append(" Abha ")
+citiy.append(" Dammam ")
+citiy.append(" Taif ")
+citiy.append(" Hail ")
+citiy.append(" Jubail ")
+
+
+var Population = [Int]()
+
+Population.append(5000)
+Population.append(1200)
+Population.append(4096)
+Population.append(1120)
+Population.append(5043)
+Population.append(3097)
+Population.append(6590)
+Population.append(5477)
+Population.append(1335)
+Population.append(7590)
+
+if(Cities == citiy[0]){
+    print(Population[0])
+}
+if(Cities == citiy[1]){
+    print(Population[1])
+}
+if(Cities == citiy[2]){
+    print(Population[2])
+}
+   
+if(Cities == citiy[3]){
+    print(Population[3])
+}
+   
+if(Cities == citiy[4]){
+    print(Population[4])
+}
+   
+if(Cities == citiy[5]){
+    print(Population[5])
+}
+   
+if(Cities == citiy[6]){
+    print(Population[6])
+}
+   
+if(Cities == citiy[7]){
+    print(Population[7])
+}
+   
+if(Cities == citiy[8]){
+    print(Population[8])
+}
+   
+if(Cities == citiy[9]){
+    print(Population[9])
+}
+   
+
---
+++ Homework4/HomeWork4:2.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var name = "Jeedh"
+var cities = ["Tabuk" , "Jeedh", "Dammam" , "Abha" ,"Madinah"]
+var Population = [400,870,783,445,509]
+if name == cities[0]{
+    print(Population[0])
+}
+if name == cities[0]{
+    print(Population[0])
+}
+if name == cities[1]{
+    print(Population[1])
+}
+if name == cities[2]{
+    print(Population[2])
+}
+if name == cities[3]{
+    print(Population[3])
+}
+if name == cities[4]{
+    print(Population[4])
+}
+
+
