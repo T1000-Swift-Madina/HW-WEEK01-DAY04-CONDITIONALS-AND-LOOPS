a4f712a9d7aeac29cfa53c0fc0456646d73d4745
Yousef Alasmar <Your github email>
2021-09-30 09:18:54 +0300

    home work 4

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,37 @@
+var city = "Makkah"
+var Cities = ["Madina" , "Makkah" , "Jeddah" , "Tabuk" , "Taif" , "Yanbu" , "Dammam" , "Hayel" , "Abha" , "Khubar"]
+var Population = [1,2,3,4,6,7,8,9,10]
+
+
+
+    
+    switch city {
+    case "Madina":
+        print(Population[0])
+        break
+    case "Makkah":
+        print(Population[1])
+    case "Jeddah":
+        print(Population[2])
+    case "Tabuk":
+        print(Population[3])
+    case "Taif":
+        print(Population[4])
+    case "Yanbu":
+        print(Population[5])
+    case "Dammam":
+        print(Population[6])
+    case "Hayel":
+        print(Population[7])
+    case "Abha":
+        print(Population[8])
+    case "Khubar":
+        print(Population[9])
+        
+    default:
+        print("non")
+        
+    }
+    
+//}
+
