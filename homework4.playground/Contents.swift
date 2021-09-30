96e551b39903c3ae4bc91e4b9ba23784effc8f55
Bndr <Bandar.Obaid.med.s@tuwaiq.edu.sa>
2021-09-30 09:37:59 +0300

    bndar

---
+++ homework4.playground/Contents.swift
@@ -0,0 +1,54 @@
+
+import UIKit
+//
+//1. Make a string variable to for selected city name
+//2. Create an empty String Array variable and name it ‘Cities’
+//3. Append it with 10 at least city names to the above list
+//4. Make another integer array of same size as above and name it as ‘Population’ with appropriate default values
+//5. Write a code that will print the population of the selected city from the list
+//6. Use all concepts like if, else, loos and switch statements as you see it appropriate.
+
+var selectedCity = "Dammam"
+
+var cities = [ "jeddah" , "riyadh" ,"medina" ,"hail" ,"tabuk" ,"abha" , "al-Qassim" , "yanbu" , "unaizah" , "Dammam"]
+var population = [ 198877,2987,3999,400,29228,92282,67777,88876,65433,99999]
+
+if selectedCity == cities[0]{
+    print("The of \(cities[0]) is \(population[0])")
+}
+else if selectedCity == cities[1] {
+    
+    print("The of \(cities[1]) is \(population[1])")
+}
+else if selectedCity == cities[2] {
+    
+    print("The of \(cities[2]) is \(population[2])")
+}
+else if selectedCity == cities[3] {
+    
+    print("The of \(cities[3]) is \(population[3])")
+}
+else if selectedCity == cities[4] {
+    
+    print("The of \(cities[4]) is \(population[4])")
+}
+else if selectedCity == cities[5] {
+    
+    print("The of \(cities[5]) is \(population[5])")
+}
+else if selectedCity == cities[6] {
+    
+    print("The of \(cities[6]) is \(population[6])")
+}
+else if selectedCity == cities[7] {
+    
+    print("The of \(cities[7]) is \(population[7])")
+}
+else if selectedCity == cities[8] {
+    
+    print("The of \(cities[8]) is \(population[8])")
+}
+else if selectedCity == cities[9] {
+    
+    print("The of \(cities[9]) is \(population[9])")
+}
