0946dc92e9447f3fea274cf6cc6b9aee07c6b0a0
nada_alansari <nada0an90@gmail.com>
2021-10-02 19:16:39 +0300

    my homework 04 haya

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,57 @@
+import UIKit
+
+var city_name = "madina"
+
+
+
+var cites = [String]()
+var pub = [Int]()
+
+cites.append("makh")
+cites.append("tabuk")
+cites.append("jeddah")
+cites.append("taif")
+cites.append("yanbu")
+cites.append("london")
+cites.append("dubi")
+cites.append("madina")
+cites.append("rabiq")
+cites.append("aula")
+
+
+pub.append(34000)
+pub.append(25000)
+pub.append(40000)
+pub.append(33000)
+pub.append(37000)
+pub.append(36000)
+pub.append(32000)
+pub.append(39000)
+pub.append(9000)
+pub.append(23000)
+
+
+if(city_name == cites[0]){
+    print(pub[0])
+}
+else if (city_name == cites[1]){
+     print(pub[1])
+}
+else if (city_name == cites[2]){
+    print(pub[2])
+}
+else if (city_name == cites[3]){
+    print(pub[3])
+}else if (city_name == cites[4]){
+    print(pub[4])
+}else if (city_name == cites[5]){
+    print(pub[5])
+}else if (city_name == cites[6]){
+    print(pub[6])
+}else if (city_name == cites[7]){
+    print(pub[7])
+}else if (city_name == cites[8]){
+    print(pub[8])
+}else if (city_name == cites[9]){
+    print(pub[9])
+}
