d025b23ac383ec8eb406d34ce26f9e5dd6f348de
reem alsubhi <Reem.Alsubi.med.s@tuwaiq.edu.sa>
2021-10-03 09:41:07 +0300

    new HW4

---
+++ text1.playground/Contents.swift
@@ -0,0 +1,81 @@
+import UIKit
+var ourcity = "Madenh"
+var citiy = [String]()
+
+citiy.append("Madenh")
+citiy.append("jedah")
+citiy.append("abha")
+citiy.append("tabuk")
+citiy.append("omluj")
+citiy.append("hail")
+citiy.append("taif")
+citiy.append("baha")
+citiy.append("alkarj")
+citiy.append("yanbu")
+
+var num = [Int]()
+num.append(5899)
+num.append(2200)
+num.append(677)
+num.append(122)
+num.append(2000)
+num.append(344)
+num.append(9889)
+num.append(700)
+num.append(987)
+num.append(500)
+  
+
+
+       for item in num {
+                  print(item)
+}
+
+
+if(ourcity == citiy[0]){
+    
+    print(num[0])
+    
+}
+else if (ourcity == citiy[1]){
+        print(num[1])
+}
+else if(ourcity == citiy[2]){
+    print(num[2])
+}
+else if(ourcity == citiy[3]){
+    print(num[3])
+}
+else if(ourcity == citiy[4]){
+    print(num[4])
+}
+else if(ourcity == citiy[5]){
+    print(num[5])
+}
+else if(ourcity == citiy[6]){
+    print(num[6])
+}
+else if(ourcity == citiy[7]){
+    print(num[7])
+}
+else if(ourcity == citiy[8]){
+    print(num[8])
+}
+else if (ourcity == citiy[9]){
+    print(num[9])
+}
+
+var x = "Abha"
+switch x {
+case "Abha" :
+    
+    print("nice city ")
+
+case "Riyadh" :
+print("big city")
+
+
+default :
+print("..")
+
+}
