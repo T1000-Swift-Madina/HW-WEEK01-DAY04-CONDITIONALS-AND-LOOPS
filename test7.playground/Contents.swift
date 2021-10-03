060934b62fe88856f246189fe61ee430b6a8eeed
shroog <shroogalhwity@gmail.com>
2021-10-03 10:58:41 +0300

    add test7

---
+++ test7.playground/Contents.swift
@@ -0,0 +1,77 @@
+import UIKit
+
+var Ourcity = "Madinah"
+var Cities = [String]()
+
+Cities.append("Madinah")
+Cities.append("Macca")
+Cities.append("Abha")
+Cities.append("Tabuk")
+Cities.append("Hail")
+Cities.append("Yanbu")
+Cities.append("Taif")
+Cities.append("Omluj")
+Cities.append("Gedah")
+Cities.append("Baha")
+var num = [Int]()
+
+
+num.append(6000)
+num.append(7000)
+num.append(3330)
+num.append(2000)
+num.append(1000)
+num.append(4000)
+num.append(30000)
+num.append(5000)
+num.append(3000)
+num.append(4500)
+// 1- USE IF STATMENT
+if(Ourcity == Cities[0]){
+    
+print(num[0])
+
+}
+
+else if(Ourcity==Cities[1]){
+    print(num[1])
+}
+else if(Ourcity == Cities[2]) {
+    print(num[2])
+}
+else if(Ourcity == Cities[3]){
+    
+    print(num[3])
+}
+else if(Ourcity == Cities[4]){
+    
+    print(num[4])
+    }
+else if(Ourcity == Cities[5]){
+    
+    print(num[5])
+    }
+else if(Ourcity == Cities[6]){
+    
+    print(num[6])
+    }
+else if(Ourcity == Cities[7]){
+    
+    print(num[7])
+    }
+else if(Ourcity == Cities[8]){
+    
+    print(num[8])
+    }
+else if(Ourcity == Cities[9]){
+    
+    print(num[9])
+    }
+
+
+
+
+
+    
+
+    <#body#>
