fc636f457dca83fc663051bc031875866c9e5df9
mohammad-alsharif <your github email>
2021-10-05 15:33:02 +0300

    Homework Day 5

--- Twaiq.playground/Contents.swift
+++ Twaiq.playground/Contents.swift
@@ -1,13 +1,33 @@
 import UIKit
 import Foundation
 
-var Selectedcity = "Riadh"
+var Selectedcity = "Makkah"
 
 var cities = ["Medanah", "Makkah", "Jeddah", "Riadh", "Abha", "Taief", "Alqaseem", "Albaha", "Negran", "Jezan"]
 
-var Population = ["1,152,991", "1,578,722", "4,082,184", "7,677,000" /* ... */]
+var Population = [":1,152,991", "1,578,722", "4,082,184", "7,677,000", "7,589,57", "5,799,70", "1,455,693", "411,888", "595,705", "1,603,659"]
 
-if Selectedcity == cities[3] {
+
+if Selectedcity == cities[0] {
+    print("The Selected city is \(cities[0]) and it's population is \(Population[0])")
+} else if Selectedcity == cities[1] {
+    print("The Selected city is \(cities[1]) and it's population is \(Population[1])")
+} else if Selectedcity == cities[2] {
+    print("The Selected city is \(cities[2]) and it's population is \(Population[2])")
+} else if Selectedcity == cities[3] {
     print("The Selected city is \(cities[3]) and it's population is \(Population[3])")
+} else if Selectedcity == cities[4] {
+    print("The Selected city is \(cities[4]) and it's population is \(Population[4])")
+} else if Selectedcity == cities[5] {
+    print("The Selected city is \(cities[5]) and it's population is \(Population[5])")
+} else if Selectedcity == cities[6] {
+    print("The Selected city is \(cities[6]) and it's population is \(Population[6])")
+} else if Selectedcity == cities[7] {
+    print("The Selected city is \(cities[7]) and it's population is \(Population[7])")
+} else if Selectedcity == cities[8] {
+    print("The Selected city is \(cities[8]) and it's population is \(Population[8])")
+} else if Selectedcity == cities[9] {
+    print("The Selected city is \(cities[9]) and it's population is \(Population[9])")
+} else {
+    print("The Selected city is \(cities[10]) and it's population is \(Population[10])")
 }
-
